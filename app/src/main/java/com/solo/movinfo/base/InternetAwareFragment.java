5da8ce1af68cb8c777f53df59c59ae68ccbdf1f9
em <mwendaeddy@gmail.com>
2018-09-24 18:41:07 +0300

    Use in-built flag to check if initial broadcasst is being sent: useful in ensuring internet connectivity changes are handled properly

--- app/src/main/java/com/solo/movinfo/base/InternetAwareFragment.java
+++ app/src/main/java/com/solo/movinfo/base/InternetAwareFragment.java
@@ -5,8 +5,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
-import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 
@@ -20,8 +18,6 @@ public abstract class InternetAwareFragment extends Fragment {
     private Snackbar mInternetConnectionSnackbar;
 
     private ConnectivityStateChangeReceiver mConnectivityStateChangeReceiver;
-    private boolean mFirstLoad; // Used by connectivity receiver to avoid taking actions
-    // if user is initially connected (on app start)
 
     // Actions to be performed when internet connection is OFF
     protected abstract void onInternetConnectivityOff();
@@ -29,21 +25,19 @@ public abstract class InternetAwareFragment extends Fragment {
     // Actions to be performed when internet connection is ON
     protected abstract void onInternetConnectivityOn();
 
-
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onStart() {
+        super.onStart();
         registerConnectivityChangeReceiver();
     }
 
     @Override
-    public void onDestroy() {
-        super.onDestroy();
+    public void onStop() {
+        super.onStop();
         unregisterConnectivityChangeReceiver();
     }
 
     private void registerConnectivityChangeReceiver() {
-        mFirstLoad = true;
         mConnectivityStateChangeReceiver = new ConnectivityStateChangeReceiver();
 
         IntentFilter intentFilter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
@@ -64,22 +58,25 @@ public abstract class InternetAwareFragment extends Fragment {
         public void onReceive(Context context, Intent intent) {
             boolean isConnected = NetworkUtils.isInternetConnected(requireContext());
 
+            if (isInitialStickyBroadcast() && isConnected) {
+                onInternetConnectivityOn();
+            }
+
+            if (!isInitialStickyBroadcast() && isConnected) {
+                Timber.d("Connected!");
+                showInternetConnectivityIsOn();
+            }
+
             if (!isConnected) {
                 Timber.d("Not Connected!");
                 showInternetConnectivityIsOff();
             }
 
-            if (isConnected && !mFirstLoad) {
-                Timber.d("Connected!");
-                showInternetConnectivityIsOn();
-            }
-
-            mFirstLoad = false;
         }
 
         /**
          * Display Snackbar if internet is OFF
-         *  and perform any actions specified in the onInternetConnectivityOff()
+         * and perform any actions specified in the onInternetConnectivityOff()
          */
         void showInternetConnectivityIsOff() {
             if (mInternetConnectionSnackbar != null && mInternetConnectionSnackbar.isShown()) {
@@ -97,7 +94,7 @@ public abstract class InternetAwareFragment extends Fragment {
 
         /**
          * Display Snackbar if internet is ON
-         *  and perform any actions specified in the onInternetConnectivityOn()
+         * and perform any actions specified in the onInternetConnectivityOn()
          */
         void showInternetConnectivityIsOn() {
             if (mInternetConnectionSnackbar != null && mInternetConnectionSnackbar.isShown()) {
